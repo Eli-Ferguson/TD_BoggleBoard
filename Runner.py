e1d613db1ecf92cf3016a61dd0d3859e38b94476
Eli-Ferguson-Pro-X <elijuddferguson@gmail.com>
2023-05-30 21:18:04 -0400

    Upd

--- Runner.py
+++ Runner.py
@@ -6,7 +6,7 @@ from Solver import Solution
 if __name__ == '__main__' :
     # Get File Paths From Args
     BoardsFile, wordsFile = sys.argv[ 1 ], sys.argv[ 2 ]
-    
+        
     # Read Boards and Words From Files
     BP = BoggleProblem( words_file=wordsFile, board_file=BoardsFile )
     
